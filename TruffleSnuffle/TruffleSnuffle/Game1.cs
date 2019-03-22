c25fbb2d653e0c593554e696441798f5d15d5229
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2019-03-22 14:57:48 +0000

    Trufle Snuffle rendering - position, rotation, projection, scale

---
+++ TruffleSnuffle/TruffleSnuffle/Game1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TruffleSnuffle
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        // Game Objects
+        GameObject player = new GameObject();
+        GameObject truffle = new GameObject();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            // Set up game window
+            graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width / 3;
+            graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height / 3;
+            graphics.ApplyChanges();
+
+            // Set up GameObjects
+            truffle.position.X = 200f;
+            truffle.rotation.X = 0.2f;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            // Load models
+            player.LoadModel(Content, "PigN");
+            truffle.LoadModel(Content, "TruffleN");
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            // Make the truffle rotate a bit each frame
+            // (really this should be scaled based on gameTime)
+            truffle.rotation.Y += 0.1f;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            // Draw GameObjects
+            player.Draw();
+            truffle.Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TruffleSnuffle/TruffleSnuffle/GameObject.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace TruffleSnuffle
+{
+    class GameObject
+    {
+
+        // Model
+        public Model model;
+        public Matrix[] transforms;
+
+        // Transform data
+        public Vector3 position = Vector3.Zero;
+        public Vector3 rotation = Vector3.Zero;
+
+
+        // Function to load our model in from file
+        public void LoadModel(ContentManager content, string name)
+        {
+            // Load in our model from the content pipeline
+            model = content.Load<Model>(name);
+
+            // From our model, copy the transforms for each mesh (in model space)
+            transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+        }
+
+        // Draws the model to the screen
+        public void Draw()
+        {
+            // Loop through the meshes in the 3D model, drawing each one in turn
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                // Loop through each effect on the mesh
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    // To render we need three things - world matrix, view matrix, and projection matrix
+                    // But we actually start in model space - this is where our world starts before transforms
+
+                    // ------------------------------
+                    // MESH BASE MATRIX
+                    // ------------------------------
+                    // Our meshes start with world = model space, so we use our transforms array
+                    effect.World = transforms[mesh.ParentBone.Index];
+
+
+                    // ------------------------------
+                    // WORLD MATRIX
+                    // ------------------------------
+                    // Transform from model space to world space in order - scale, rotation, translation.
+
+                    // 1. Scale (TODO)
+
+                    // 2. Rotation
+                    // Rotate our model in the game world
+                    effect.World *= Matrix.CreateRotationX(rotation.X); // Rotate around the x axis
+                    effect.World *= Matrix.CreateRotationY(rotation.Y); // Rotate around the y axis
+                    effect.World *= Matrix.CreateRotationZ(rotation.Z); // Rotate around the z axis
+
+                    // 3. Translation / position
+                    // Move our model to the correct place in the game world
+                    effect.World *= Matrix.CreateTranslation(position);
+
+                    // ------------------------------
+                    // VIEW MATRIX
+                    // ------------------------------
+                    // This puts the model in relation to where our camera is, and the direction of our camera.
+                    effect.View = Matrix.CreateLookAt(
+                        new Vector3(0f, 300f, -400f),
+                        Vector3.Zero,
+                        Vector3.Up
+                        );
+
+
+                    // ------------------------------
+                    // PROJECTION MATRIX
+                    // ------------------------------
+                    // Projection changes from view space (3D) to screen space (2D)
+                    // Can be either orthographic or perspective
+
+                    // Perspective
+                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(
+                        MathHelper.ToRadians(45f),
+                        16f / 9f,
+                        1f,
+                        10000f);
+
+                    // Orthographic
+                    //effect.Projection = Matrix.CreateOrthographic(
+                    //    1600, 900, 1f, 10000f
+                    //    );
+
+                    // ------------------------------
+                    // LIGHTING
+                    // ------------------------------
+                    // Some basic lighting, feel free to tweak and experiment
+                    effect.LightingEnabled = true;
+                    effect.Alpha = 1.0f;
+                    effect.AmbientLightColor = new Vector3(1.0f);
+
+                }
+
+                // Draw the current mesh using the effects we set up
+                mesh.Draw();
+            }
+        }
+
+
+    }
+}
